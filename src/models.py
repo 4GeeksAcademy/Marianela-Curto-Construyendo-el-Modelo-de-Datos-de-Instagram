d2969b5dded5c0c0ff8e7d14890f22fcccf2c9f4
marianelacurto <marianelacurto@gmail.com>
2026-07-01 23:38:32 +0000

    add database models and relationships

--- src/models.py
+++ src/models.py
@@ -1,19 +1,96 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
+from typing import List
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(nullable=False)
+    firstname: Mapped[str] = mapped_column(nullable=False)
+    lastname: Mapped[str] = mapped_column(nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+  
+    posts: Mapped[List["Post"]] = relationship(back_populates="user")
+    comments: Mapped[List["Comment"]] = relationship(back_populates="user")
+    
+    followers: Mapped[List["Follower"]] = relationship(
+        back_populates="user_to",
+        foreign_keys="Follower.user_to_id"
+        )
+    following: Mapped[List["Follower"]] = relationship(
+        back_populates="user_from",
+        foreign_keys="Follower.user_from_id"
+        )
 
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "firstname": self.firstname,
+            "lastname": self.lastname,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+class Comment(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    comment_text: Mapped[str] = mapped_column(nullable=False)
+    author_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"), nullable=False)
+       
+    user: Mapped["User"] = relationship(back_populates="comments")
+    post: Mapped["Post"] = relationship(back_populates="comments")   
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "comment_text": self.comment_text,
+            "author_idt": self.author_id,
+            "post_id": self.post_id,
             # do not serialize the password, its a security breach
         }
+    
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    text: Mapped[str] = mapped_column(nullable=False)
+    url: Mapped[str] = mapped_column(nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+
+    user: Mapped["User"] = relationship(back_populates="posts")
+    comments: Mapped[List["Comment"]] = relationship(back_populates="post")  
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "text": self.text,
+            "user_id": self.ser_id,
+            # do not serialize the password, its a security breach
+        }    
+    
+class Follower(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_from_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    user_to_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+   
+    user_from: Mapped["User"] = relationship(
+        back_populates="following",
+        foreign_keys=[user_from_id]
+    )
+
+    user_to: Mapped["User"] = relationship(
+        back_populates="followers",
+        foreign_keys=[user_to_id]
+    )  
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_from_id": self.user_from_id,
+            "user_to_id": self.user_to_id,
+            # do not serialize the password, its a security breach
+        }        
